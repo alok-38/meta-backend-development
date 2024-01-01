ed7b8ebbb7f32e050d653e6c2cefef06b95d8211
Alokananda Y <plantsfortrees@gmail.com>
2024-01-01 15:58:43 +0530

    Trying out simple JS

---
+++ introduction-to-html_and_css/html-documents/script.js
@@ -0,0 +1,11 @@
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const html = document.querySelector('html');
+
+info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+
+document.body.appendChild(info);
+
+html.onclick = function() {
+    const listItem = document.createElement('li');
+}
